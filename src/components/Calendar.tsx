0a6b473cbe652759507f74449bb85b1970a66ed5
Mariano <marianocapiliku@gmail.com>
2024-03-21 21:35:16 +0100

    Update CalendarDay component

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -34,31 +34,7 @@ const Calendar = () => {
 
         <div className="days">
           {calendarDays.map((day) => (
-            <div
-              key={day.getTime()}
-              className="day non-month-day old-month-day"
-            >
-              <div className="day-header">
-                <div className="week-name">Sun</div>
-                <div className="day-number">28</div>
-                <button className="add-event-btn">+</button>
-              </div>
-              <div className="events">
-                <button className="all-day-event blue event">
-                  <div className="event-name">Short</div>
-                </button>
-                <button className="all-day-event green event">
-                  <div className="event-name">
-                    Long Event Name That Just Keeps Going
-                  </div>
-                </button>
-                <button className="event">
-                  <div className="color-dot blue"></div>
-                  <div className="event-time">7am</div>
-                  <div className="event-name">Event Name</div>
-                </button>
-              </div>
-            </div>
+            <CalendarDay key={day.getTime()} />
           ))}
         </div>
       </div>
@@ -68,4 +44,31 @@ const Calendar = () => {
 
 export default Calendar;
 
-const CalendarDay = () => {};
+const CalendarDay = () => {
+  return (
+    <>
+      <div className="day non-month-day old-month-day">
+        <div className="day-header">
+          <div className="week-name">Sun</div>
+          <div className="day-number">28</div>
+          <button className="add-event-btn">+</button>
+        </div>
+        <div className="events">
+          <button className="all-day-event blue event">
+            <div className="event-name">Short</div>
+          </button>
+          <button className="all-day-event green event">
+            <div className="event-name">
+              Long Event Name That Just Keeps Going
+            </div>
+          </button>
+          <button className="event">
+            <div className="color-dot blue"></div>
+            <div className="event-time">7am</div>
+            <div className="event-name">Event Name</div>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};

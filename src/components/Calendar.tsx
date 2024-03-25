d716084a84c07c114df67deaeff31f89b85e3b31
Mariano <marianocapiliku@gmail.com>
2024-03-25 14:20:20 +0100

    Refactoring Calendar component split the Calendar into diffrents components

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -5,16 +5,12 @@ import {
   endOfWeek,
   endOfMonth,
   eachDayOfInterval,
-  isSameMonth,
-  isBefore,
-  endOfDay,
-  isToday,
   addMonths,
   subMonths,
 } from "date-fns";
 import "../styles.css";
 import { formatDate } from "../utils/formatDate";
-import { cc } from "../utils/cc";
+import { CalendarDay } from "./CalendarDay";
 
 const Calendar = () => {
   const [selectedMonth, setSelectedMonth] = useState(new Date());
@@ -85,53 +81,3 @@ const Calendar = () => {
 };
 
 export default Calendar;
-
-type CalendarDayProps = {
-  day: Date;
-  showWeekName: boolean;
-  selectedMonth: Date;
-};
-const CalendarDay = ({
-  day,
-  selectedMonth,
-  showWeekName,
-}: CalendarDayProps) => {
-  return (
-    <>
-      <div
-        className={cc(
-          "day",
-          !isSameMonth(day, selectedMonth) && "non-month-day",
-          isBefore(endOfDay(day), new Date()) && "old-month-day"
-        )}
-      >
-        <div className="day-header">
-          {showWeekName && (
-            <div className="week-name">
-              {formatDate(day, { weekday: "short" })}
-            </div>
-          )}
-          <div className={cc("day-number", isToday(day) && "today")}>
-            {formatDate(day, { day: "numeric" })}
-          </div>
-          <button className="add-event-btn">+</button>
-        </div>
-        {/* <div className="events">
-          <button className="all-day-event blue event">
-            <div className="event-name">Short</div>
-          </button>
-          <button className="all-day-event green event">
-            <div className="event-name">
-              Long Event Name That Just Keeps Going
-            </div>
-          </button>
-          <button className="event">
-            <div className="color-dot blue"></div>
-            <div className="event-time">7am</div>
-            <div className="event-name">Event Name</div>
-          </button>
-        </div> */}
-      </div>
-    </>
-  );
-};
---
+++ src/components/CalendarDay.tsx
@@ -0,0 +1,53 @@
+import { isSameMonth, isBefore, endOfDay, isToday } from "date-fns";
+import { formatDate } from "../utils/formatDate";
+import { cc } from "../utils/cc";
+
+type CalendarDayProps = {
+  day: Date;
+  showWeekName: boolean;
+  selectedMonth: Date;
+};
+export const CalendarDay = ({
+  day,
+  selectedMonth,
+  showWeekName,
+}: CalendarDayProps) => {
+  return (
+    <>
+      <div
+        className={cc(
+          "day",
+          !isSameMonth(day, selectedMonth) && "non-month-day",
+          isBefore(endOfDay(day), new Date()) && "old-month-day"
+        )}
+      >
+        <div className="day-header">
+          {showWeekName && (
+            <div className="week-name">
+              {formatDate(day, { weekday: "short" })}
+            </div>
+          )}
+          <div className={cc("day-number", isToday(day) && "today")}>
+            {formatDate(day, { day: "numeric" })}
+          </div>
+          <button className="add-event-btn">+</button>
+        </div>
+        {/* <div className="events">
+          <button className="all-day-event blue event">
+            <div className="event-name">Short</div>
+          </button>
+          <button className="all-day-event green event">
+            <div className="event-name">
+              Long Event Name That Just Keeps Going
+            </div>
+          </button>
+          <button className="event">
+            <div className="color-dot blue"></div>
+            <div className="event-time">7am</div>
+            <div className="event-name">Event Name</div>
+          </button>
+        </div> */}
+      </div>
+    </>
+  );
+};
